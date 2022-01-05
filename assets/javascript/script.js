32a16b9fcf19bd617684299356325a5c4e5c1e17
Amber Robeck <arr5533@gmail.com>
2022-01-04 21:39:18 -0600

    fixed the link for font awesome in html, added urlRoot variable for api root, added userSelect to collect user input, concated urlRoot and userSelect for ajax call, deleted console logs not being used

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,24 @@
-$.ajax({
-    url:"https:api.uberchord.com/v1/chords/F_maj7",
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-})
-
+var userSelect = "";
+var urlRoot = "https:api.uberchord.com/v1/chords/";
 var button = $(".button")
-button.on("click", function(){
-    console.log("working")
-
-var dditem = $(".dropdown-menu")
-dditem.toggle('is-active');
-})
 var ddContent = $(".dropdown-content")
-ddContent.on("click", function(event){
-    //event.target
-    console.log(event.target.innerText)
-
-    var dditem = $("dropdown-item")
-    //console.log(dditem.val)
 
 
+button.on("click", function () {
+    var dditem = $(".dropdown-menu")
+    dditem.toggle('is-active');
+})
+
+
+ddContent.on("click", function (event) {
+    userSelect = event.target.innerText
+    console.log(userSelect)
+
+    $.ajax({
+        url: urlRoot + userSelect,
+        // url: "https:api.uberchord.com/v1/chords/F_maj7",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
 })
