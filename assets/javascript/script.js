0a866610346571fee8b6b3ffd64ce1c22f262c05
Amber Robeck <arr5533@gmail.com>
2022-01-06 14:35:16 -0600

    changed fetch into own function, changed getLocalS and saveLocalS to display either last chord selected or a C chord

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -24,6 +24,33 @@ ddContent.on("click", function (event) {
     dditem.toggle('is-active');
     saveLocalS(userSelect);
     var insBtn = $('#scapiobjid1').attr('chord', userSelect)
+    getApi();
+
+    // fetch("https://api.uberchord.com/v1/chords/" + userSelect.slice(0, 1))
+    //     .then(function (response) {
+    //         return response.json();
+    //         // console.log(response.json());
+    //     }).then(function (data) {
+    //         console.log(data)
+    //         //number of what finger to use on each string
+    //         console.log(data[0].fingering)
+    //         //what strings are being pressed on what fret down to make chord
+    //         console.log(data[0].strings)
+    //         //adding api data to user screen
+    //         userSelectBox.text("Fingering style: " + data[0].fingering + " Strings: " + data[0].strings)
+    //         //Adding user select fingering diagram icon to page
+    //         // var chordIcon = $("#icon")
+    //         // $(chordIcon).attr("src", "https://chordify.net/img/diagrams/guitar/" + userSelect + ".png");
+    //         var chordIcon = $("#icon")
+    //         $(chordIcon).attr("src", "assets/images/chordImg/" + userSelect.slice(0, 1) + ".png");
+
+
+    //     })
+})
+
+function getApi() {
+
+
 
     fetch("https://api.uberchord.com/v1/chords/" + userSelect.slice(0, 1))
         .then(function (response) {
@@ -45,7 +72,7 @@ ddContent.on("click", function (event) {
 
 
         })
-})
+}
 
 var local = [];
 function saveLocalS(info) {
@@ -61,14 +88,17 @@ function saveLocalS(info) {
 var getStorage = [];
 function getLocalS() {
     if (localStorage.getItem("FavChord")) {
-        var saveChord = $("<div>");
         var getStorage = JSON.parse(localStorage.getItem("FavChord"));
-        userSelectBox.append(saveChord)
+        var saveChord = $("<div>");
         saveChord.text("current saved " + getStorage[0]);
-        console.log(typeof getStorage);
-        console.log(getStorage[0]);
+        userSelectBox.append(saveChord)
+        // console.log(typeof getStorage);
+        // console.log(getStorage[0]);
+        userSelect = JSON.parse(localStorage.getItem("FavChord"))
+        getApi(userSelect);
     } else {
-        // getStorage = ["C"]
+        userSelect = "C"
+        getApi(userSelect);
         // var saveChord = $("<div>");
         // saveChord.text("current saved " + getStorage[0]);
         console.log("you don't have any favorites")
