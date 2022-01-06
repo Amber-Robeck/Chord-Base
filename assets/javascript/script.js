bf9eaaaec0527e1cb3f715b665b8e656aecc0a1a
Amber Robeck <arr5533@gmail.com>
2022-01-05 21:46:16 -0600

    added toggle change to dditem in ddcontent on click, deleted unused lines of code and comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,13 @@
 var userSelect = "";
-// var urlRootEl = "https:api.uberchord.com/v1/chords/";
 var button = $(".button")
 var ddContent = $(".dropdown-content")
 var userSelectBox = $("#userSelectBox")
+var dditem = $(".dropdown-menu")
 
 // $(<"p">)
 userSelectBox.text("It's working")
 
 button.on("click", function () {
-    var dditem = $(".dropdown-menu")
     dditem.toggle('is-active');
 })
 
@@ -16,6 +15,7 @@ button.on("click", function () {
 ddContent.on("click", function (event) {
     userSelect = event.target.innerText
     console.log(userSelect)
+    dditem.toggle('is-active');
 
     fetch("https://api.uberchord.com/v1/chords/" + userSelect.slice(0, 1))
         .then(function (response) {
@@ -29,15 +29,10 @@ ddContent.on("click", function (event) {
             console.log(data[0].strings)
             //adding api data to user screen
             userSelectBox.text("Fingering style: " + data[0].fingering + " Strings: " + data[0].strings)
-            //adding userselect to img currently only working on c and f as they are the only img this site has
-            //TODO: find a source for all images
+            //Adding user select fingering diagram icon to page
             var chordIcon = $("#icon")
-            // $(chordIcon).attr("src", "https://api.uberchord.com/examples/" + userSelect + ".svg");
             $(chordIcon).attr("src", "https://chordify.net/img/diagrams/guitar/" + userSelect + ".png");
-            // https://chordify.net/img/diagrams/guitar/A_maj.png
 
-            //alert(userSelect.slice(0, 1));
-            // alert(userSelect.substring(1));
         })
 })
 
